b5dddde7762d8c766c7cc71f33a86fa3208b0a5a
Code-Nature <codenature01001@gmail.com>
2023-02-22 10:44:05 -0500

    maquetacion y estilos del home

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/components/Header.css'
+
+function Header() {
+  return (
+    <div className='Header'>
+      <h1>PlatziConf Merge</h1>
+      <div className="Header-checkout">
+        CheckOut
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,19 +6,22 @@ import Information from '../containers/Information';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/checkout/information" element={<Information />} />
-        <Route path="/checkout/payment" element={<Payment />} />
-        <Route path="/checkout/success" element={<Success />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/information" element={<Information />} />
+          <Route path="/checkout/payment" element={<Payment />} />
+          <Route path="/checkout/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 }
 
